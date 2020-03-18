2fcaacf37a470e227f2478ede115ae68f9b1351e
CatherineStrassburg <catherinestrassburg@CatheriburgsMBP.hsd1.ma.comcast.net>
2020-03-17 21:16:12 -0400

    Made to push to pi

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `${ __dirname }/test.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: true,
+});
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment-timezone');
+const $ = require('jsrender');
+const fs = require('fs');
+
 const app = express();
 const port = 8080;
 
@@ -8,7 +20,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-    response.send('Hello from Express!');
+    //response.send('Hello from Express!');
+    myCamera.snap()
+    .then((result) => {
+        // Your picture was captured
+        fs.readFile(`${ __dirname }/test.jpg`, function (err, content) {
+            if (err) {
+                response.writeHead(400, {'Content-type':'text/html'})
+                console.log(err);
+                response.end("No such image");    
+            } else {
+                //specify the content type in the response will be an image
+                response.writeHead(200,{'Content-type':'image/jpg'});
+                response.end(content);
+            }
+    })
+    .catch((error) => {
+     // Handle your error
+    });
     console.log('/');
 })
 
